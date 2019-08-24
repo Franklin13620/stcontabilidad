3b1e9a9daf5eebd87197e4862f9f5e2f1ef59e10
Franklin13620 <franklintorres13620@gmail.com>
2019-08-24 16:21:42 -0600

    Initial Commit

--- tmp/wK4Jh9SR56DQ7yfcqpbv/cache/073af1a77d07e9554fcf2bec8a70de92.php
+++ tmp/wK4Jh9SR56DQ7yfcqpbv/cache/073af1a77d07e9554fcf2bec8a70de92.php
@@ -1,1 +1,1 @@
-a:6:{i:0;s:8:"fs_users";i:1;s:8:"fs_pages";i:2;s:7:"empresa";i:3;s:7:"fs_vars";i:4;s:7:"divisas";i:5;s:14:"fs_extensions2";}
+a:13:{i:0;s:8:"fs_users";i:1;s:8:"fs_pages";i:2;s:7:"empresa";i:3;s:7:"fs_vars";i:4;s:7:"divisas";i:5;s:14:"fs_extensions2";i:6;s:7:"fs_logs";i:7;s:9:"almacenes";i:8;s:12:"cuentasbanco";i:9;s:10:"ejercicios";i:10;s:10:"formaspago";i:11;s:6:"paises";i:12;s:6:"series";}
